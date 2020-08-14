230d72906a48f08755a973b481b10d9fc05a6e57
Ömer Ertekin <57711950+OmerErtekin@users.noreply.github.com>
2020-08-14 14:33:10 +0300

    Add files via upload

---
+++ Scripts/Item/Blade.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    private Rigidbody2D bladeRB;
+    private PlayerMove playerScript;
+    private SpriteRenderer bladeRenderer;
+    private bool canGiveDamage = true;
+
+    
+    void Start()
+    {
+        bladeRB = GetComponent<Rigidbody2D>();
+        playerScript = GameObject.Find("Player").GetComponent<PlayerMove>();
+        bladeRenderer = GetComponent<SpriteRenderer>();
+    }
+
+   
+    void Update()
+    {
+        transform.Rotate(0, 0, Time.time * -50);
+        StartCoroutine(GoToPlayer());
+        
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && canGiveDamage == true)
+        {
+                StartCoroutine(giveDamage());
+            
+        }
+    }
+    IEnumerator GoToPlayer()
+    {
+        bladeRB.velocity = new Vector2(-5, 0);
+        yield return new WaitForSeconds(3f);
+        bladeRenderer.enabled = false;
+        transform.position = new Vector3(transform.position.x +15, transform.position.y, transform.position.z);
+        bladeRenderer.enabled = true;
+        this.gameObject.SetActive(false);
+    }
+
+    IEnumerator giveDamage()
+    {
+        canGiveDamage = false;
+        playerScript.TakeDamage(50);
+        yield return new WaitForSeconds(1f);
+        canGiveDamage = true;
+    }
+}
---
+++ Scripts/Item/Train.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Train : MonoBehaviour
+{
+    private PlayerMove playerScript;
+    public Transform mineTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = GameObject.Find("Player").GetComponent<PlayerMove>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerScript.isTimeToGo == true)
+        {
+            StartCoroutine(GoNextLevel());
+        }
+
+    }
+
+    IEnumerator GoNextLevel()
+    {
+        playerScript.canMove = false;
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(mineTransform.position.x, transform.position.y), 2.5f * Time.deltaTime);
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Level 2");
+
+    }
+    
+}
